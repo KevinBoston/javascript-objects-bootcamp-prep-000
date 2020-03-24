838e7ae7bddcf4630aa903c54462e88d4b875fa9
Kevin Boston <kbos22@gmail.com>
2020-03-24 18:06:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist (playlist, artist, song){
   playlist[artist] = song;
   return playlist;
 }
-function removeFromPlaylist (pList, artistName){
-  delete pList[artist];
-  return pList;
+function removeFromPlaylist (playlist, artist){
+  delete playlist[artist];
+  return playlist;
 }
